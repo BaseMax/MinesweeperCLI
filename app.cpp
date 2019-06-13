fe62ab133160dd0b866be93195b04b77f67581a5
Max Base <MaxBaseCode@Gmail.Com>
2019-06-13 16:43:40 +0430

    Update app.cpp

--- app.cpp
+++ app.cpp
@@ -1,1 +1,10 @@
-
+/**
+*
+* @Name : MinesweeperCLI/app.cpp
+* @Version : 1.0
+* @Programmer : Max
+* @Date : 2018-07-13
+* @Released under : https://github.com/BaseMax/MinesweeperCLI/blob/master/LICENSE
+* @Repository : https://github.com/BaseMax/MinesweeperCLI
+*
+**/
